309aa7d8519004bd8f300b7e0191f084aaaf6f97
Dhvanit <monparadhvanit@gmail.com>
2024-10-20 12:26:52 +0530

    configured nextauth with signin functionality and middlewares

---
+++ src/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,97 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import FacebookProvider from "next-auth/providers/facebook";
+import bcryptjs from "bcryptjs";
+import connectDB from "@/lib/connectDB";
+import UserModel from "@/model/user.model";
+
+interface Credentials {
+  identifier: string;
+  password: string;
+}
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "Credentials",
+      name: "Credentials",
+      credentials: {
+        identifier: { label: "Email or Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      } as Record<keyof Credentials, { label: string; type: string }>,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      async authorize(credentials: Credentials | undefined): Promise<any> {
+        if (!credentials) {
+          throw new Error("Missing credentials");
+        }
+
+        await connectDB();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("Invalid login credentials");
+          }
+
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before logging in");
+          }
+
+          const isPasswordCorrect = await bcryptjs.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Invalid login credentials");
+          }
+        } catch (error) {
+          throw new Error("An error occurred during authentication", error as ErrorOptions);
+        }
+      },
+    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    // }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    // }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.username = token.username;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id;
+        token.isVerified = user.isVerified;
+        token.username = user.username;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
--- src/api/signup/route.ts
+++ src/api/signup/route.ts
@@ -7,10 +7,12 @@ export async function POST(request: Request) {
   await connectDB();
   try {
     const { username, email, password } = await request.json();
+
     const existingUserVerifiedByUsername = await UserModel.findOne({
       username,
       isVerified: true,
     });
+
     if (existingUserVerifiedByUsername) {
       return Response.json(
         {
@@ -20,8 +22,10 @@ export async function POST(request: Request) {
         { status: 400 }
       );
     }
+
     const existingUserByEmail = await UserModel.findOne({ email });
     const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
     if (existingUserByEmail) {
       if (existingUserByEmail.isVerified) {
         return Response.json(
@@ -31,7 +35,7 @@ export async function POST(request: Request) {
           },
           { status: 400 }
         );
-      } else {
+      } else {  
         const hashedPassword = await bcryptjs.hash(password, 10);
         existingUserByEmail.password = hashedPassword;
         existingUserByEmail.verifyCode = verifyCode;
@@ -56,6 +60,7 @@ export async function POST(request: Request) {
 
       await newUser.save();
     }
+
     const emailResponse = await sendVerificationEmail(
       email,
       username,
@@ -68,6 +73,7 @@ export async function POST(request: Request) {
         { status: 500 }
       );
     }
+
     return Response.json(
       { success: true, message: "User registered successfully" },
       { status: 201 }
